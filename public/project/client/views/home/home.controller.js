4063b6cfb35d88c9b497eb853f13f3206b48e143
Dhruv Shah <dhruvsshah@gmail.com>
2016-03-02 12:52:25 -0500

    Added home.controller.js to set up data retrieving from the server

---
+++ public/project/client/views/home/home.controller.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Dhruv on 3/2/2016.
+ */
+(function() {
+    angular
+        .module("RentOutApp")
+        .controller("HomeController",HomeController);
+
+    function HomeController($scope, $location, $http) {
+        var SEARCH_URL = "https://api.9flats.com/api/v4/places?client_id=9SDO9JGSYZiwc9S89yjW5c883Lbj0AopNdVnhS3l&search[query]=SEARCHQUERY";
+        $scope.searchPlaces = searchPlaces;
+
+        function searchPlaces(search) {
+            var url = SEARCH_URL.replace("SEARCHQUERY",search.query);
+            $http.get(url)
+                .success(renderDetails);
+        }
+
+        function  renderDetails(response) {
+            console.log(response);
+            $scope.displayResponse = response;
+        }
+    }
+})();
--- public/project/config.js
+++ public/project/config.js
@@ -12,30 +12,31 @@
                     redirectTo: "/home"
 
                 })
-            .when("/profile", {
-                    templateUrl: "views/users/profile.view.html",
-                    controller: "ProfileController"
-                })
-            .when("/admin", {
-                    templateUrl: "views/admin/admin.view.html"
-                })
-            .when("/forms", {
-                    templateUrl: "views/forms/forms.view.html"
-                })
-            .when("/fields", {
-                    templateUrl: "views/forms/fields.view.html"
-                })
-            .when("/register", {
-                    templateUrl: "views/users/register.view.html",
-                    controller: "RegisterController"
-                })
-            .when("/login", {
-                    templateUrl: "views/users/login.view.html",
-                    controller: "LoginController"
-                })
+            //.when("/profile", {
+            //        templateUrl: "views/users/profile.view.html",
+            //        controller: "ProfileController"
+            //    })
+            //.when("/admin", {
+            //        templateUrl: "views/admin/admin.view.html"
+            //    })
+            //.when("/forms", {
+            //        templateUrl: "views/forms/forms.view.html"
+            //    })
+            //.when("/fields", {
+            //        templateUrl: "views/forms/fields.view.html"
+            //    })
+            //.when("/register", {
+            //        templateUrl: "views/users/register.view.html",
+            //        controller: "RegisterController"
+            //    })
+            //.when("/login", {
+            //        templateUrl: "views/users/login.view.html",
+            //        controller: "LoginController"
+            //    })
             .when("/home", {
-                    templateUrl: "views/home/home.view.html"
-                })
+                templateUrl: "client/views/home/home.view.html",
+                controller: "HomeController"
+            })
             .otherwise({
                     redirectTo: "/"
                 })
