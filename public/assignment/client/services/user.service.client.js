b2d0b5a96f4ad14224795bf35248efdb2d9290f3
Dhruv Shah <dhruvsshah@gmail.com>
2016-03-18 13:42:18 -0400

    Refactored user.service.client.js

--- public/assignment/client/services/user.service.client.js
+++ public/assignment/client/services/user.service.client.js
@@ -7,21 +7,7 @@
         .module("FormBuilderApp")
         .factory("UserService",UserService)
 
-    function UserService($rootScope) {
-
-        //var users = [
-        //    {	"_id":123, "firstName":"Alice",            "lastName":"Wonderland",
-        //        "username":"alice",  "password":"alice",   "roles": ["student"], "email":""		},
-        //    {	"_id":234, "firstName":"Bob",              "lastName":"Hope",
-        //        "username":"bob",    "password":"bob",     "roles": ["admin"], "email":""		},
-        //    {	"_id":345, "firstName":"Charlie",          "lastName":"Brown",
-        //        "username":"charlie","password":"charlie", "roles": ["faculty"], "email":""		},
-        //    {	"_id":456, "firstName":"Dan",              "lastName":"Craig",
-        //        "username":"dan",    "password":"dan",     "roles": ["faculty", "admin"], "email":""},
-        //    {	"_id":567, "firstName":"Edward",           "lastName":"Norton",
-        //        "username":"ed",     "password":"ed",      "roles": ["student"], "email":""		}
-        //];
-
+    function UserService($rootScope,$http) {
 
         var api = {
             findUserByCredentials: findUserByCredentials,
@@ -35,49 +21,27 @@
         return api;
 
         function findUserByCredentials(username, password) {
-            //for(var index=0;index<users.length;index++) {
-            //    if(users[index].username == username) {
-            //        if(users[index].password==password) {
-            //            callback(users[index]);
-            //        }
-            //    }
-            //}
-            //callback(null);
             return $http.get("/api/assignment/user?username="+username+"&password="+password);
         }
 
-        function findAllUsers(callback){
-            return callback(users);
+        function findUserByUsername(username) {
+            return $http.get("/api/assignment/user?username="+username);
         }
 
-        function createUser(user,callback) {
-            user._id = (new Date()).getTime();
-            users[users.length] = user;
-            callback(user);
+        function findAllUsers(){
+            return $http.get("/api/assignment/user");
         }
 
-        function deleteUserById(userId,callback) {
-            for(var index=0;index<users.length;index++) {
-                if(users[index]._id == userId) {
-                    users.remove(index);
-                }
-            }
-            callback(users);
-
+        function createUser(user) {
+            return $http.post("/api/assignment/user",user);
         }
 
-        function updateUser(userId, user, callback) {
-            for(var index=0;index<users.length;index++) {
-                if(users[index]._id == userId) {
-                    users[index].firstName = user.firstName;
-                    users[index].lastName = user.lastName;
-                    users[index].password = user.password;
-                    users[index].roles = user.roles;
-                    users[index].username = user.username;
-                    users[index].email = user.email;
-                }
-            }
-            callback(user);
+        function deleteUserById(userId) {
+           return $http.delete ("/api/assignment/user/"+userId);
+        }
+
+        function updateUser(userId, user) {
+            return $http.put("/api/assignment/user/"+userId,user);
         }
 
         function setCurrentUser(aUser) {
