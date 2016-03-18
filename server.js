f539794cd1bc7afd8defb91d9e5b7866c8943772
Dhruv Shah <dhruvsshah@gmail.com>
2016-03-18 13:05:46 -0400

    Changes in server.js to require the app.js /assignment/server

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var http = require('https');
 var app = express();
+var bodyParser    = require('body-parser');
+var multer        = require('multer');
+var cookieParser  = require('cookie-parser');
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(cookieParser());
 var SEARCH_QUERY_URL = "https://api.9flats.com/api/v4/places?client_id=9SDO9JGSYZiwc9S89yjW5c883Lbj0AopNdVnhS3l&search[query]=SEARCHQUERY";
 var SEARCH_BY_ID_URL = "https://api.9flats.com/api/v4/places/PLACEID?&client_id=9SDO9JGSYZiwc9S89yjW5c883Lbj0AopNdVnhS3l";
 
+require("./public/assignment/server/app.js")(app);
 //List of Apartments for a given query
 app.get('/api/search/place/:query', function(req, res){
     var url = SEARCH_QUERY_URL.replace("SEARCHQUERY",req.params.query);
