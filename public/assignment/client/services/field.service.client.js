d9996855233df4d2b37b66d9a27fbc1c78cb5cde
Dhruv Shah <dhruvsshah@gmail.com>
2016-03-18 14:20:24 -0400

    Added field.service.client.js

---
+++ public/assignment/client/services/field.service.client.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Dhruv on 3/18/2016.
+ */
+"use strict";
+(function() {
+    angular
+        .module("FormBuilderApp")
+        .factory("FieldService",FieldService)
+
+    function FieldService($http) {
+
+        var api = {
+            createFieldForForm:createFieldForForm,
+            getFieldsForForm:getFieldsForForm,
+            getFieldForForm: getFieldForForm,
+            deleteFieldFromForm:deleteFieldFromForm,
+            updateField:updateField
+        };
+
+        return api;
+
+        function createFieldForForm(formId, filed) {
+            return $http.post("/api/assignment/form/"+formId+"/field",filed);
+        }
+
+        function getFieldsForForm(formId) {
+            return $htpp.get("/api/assignment/form/"+formId+"/field");
+        }
+
+        function getFieldForForm(formId, fieldId) {
+            return $http.get("/api/assignment/form/"+formId+"/field/"+fieldId);
+        }
+
+        function deleteFieldFromForm(formId,fieldId) {
+            return $htpp.delete("/api/assignment/form/"+formId+"/field/"+fieldId)
+        }
+
+        function updateField(formId,fieldId, field) {
+            return $http.put("/api/assignment/form/"+formId+"/field/"+fieldId,field);
+        }
+
+    }
+
+})();
