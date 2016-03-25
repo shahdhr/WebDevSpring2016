6e72ba1d2be0b4e1f66c6bac77a6a0ecc0b85a30
Dhruv Shah <dhruvsshah@gmail.com>
2016-03-25 13:40:41 -0400

    Added all previous files of project to the client folder -Creater server directory and app.js

--- public/project/client/config.js
+++ public/project/client/config.js
@@ -24,39 +24,39 @@
                 //    })
 
                 .when("/home", {
-                    templateUrl: "client/views/home/home.view.html",
+                    templateUrl: "views/home/home.view.html",
                     controller: "HomeController"
                 })
                 .when("/details/:apartmentId", {
-                    templateUrl: "client/views/apartment/details.view.html",
+                    templateUrl: "views/apartment/details.view.html",
                     controller: "ApartmentDetailsController"
                 })
                 .when("/login", {
-                    templateUrl: "client/views/users/login.view.html",
+                    templateUrl: "views/users/login.view.html",
                     controller: "LoginController"
                 })
                 .when("/register", {
-                    templateUrl: "client/views/users/register.view.html",
+                    templateUrl: "views/users/register.view.html",
                     controller: "RegisterController"
                 })
                 .when("/profile", {
-                    templateUrl: "client/views/users/profile.view.html",
+                    templateUrl: "views/users/profile.view.html",
                     controller: "ProfileController"
                 })
                 .when("/apartment", {
-                    templateUrl: "client/views/apartment/apartment.view.html",
+                    templateUrl: "views/apartment/apartment.view.html",
                     controller: "ApartmentController"
                 })
                 .when("/booking", {
-                    templateUrl: "client/views/booking/booking.view.html",
+                    templateUrl: "views/booking/booking.view.html",
                     controller: "BookingController"
                 })
                 .when("/review", {
-                    templateUrl: "client/views/review/review.view.html",
+                    templateUrl: "views/review/review.view.html",
                     controller: "ReviewController"
                 })
                 .when("/admin", {
-                    templateUrl: "client/views/admin/admin.view.html",
+                    templateUrl: "views/admin/admin.view.html",
                     controller: "AdminController"
                 })
                 .otherwise({
