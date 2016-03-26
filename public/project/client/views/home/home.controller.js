577bb390456539982217fdc072e38487920bc2a1
Dhruv Shah <dhruvsshah@gmail.com>
2016-03-26 02:24:26 -0400

    Home Page : removed $scope and replaced with vm

--- public/project/client/views/home/home.controller.js
+++ public/project/client/views/home/home.controller.js
@@ -6,10 +6,11 @@
         .module("RentOutApp")
         .controller("HomeController",HomeController);
 
-    function HomeController($scope, $location, $http, ApartmentService) {
+    function HomeController( $location, $http, ApartmentService) {
+        var vm = this;
         var SEARCH_URL = "https://api.9flats.com/api/v4/places?client_id=9SDO9JGSYZiwc9S89yjW5c883Lbj0AopNdVnhS3l&search[query]=SEARCHQUERY";
-        $scope.searchPlaces = searchPlaces;
-        $scope.findDetailsById = findDetailsById;
+        vm.searchPlaces = searchPlaces;
+        vm.findDetailsById = findDetailsById;
         function searchPlaces(search) {
             var query = search.place;
             ApartmentService.findApartmentsByQuery(query,renderDetails);
@@ -18,7 +19,7 @@
 
         function  renderDetails(response) {
             console.log(response);
-            $scope.places = response.places;
+            vm.places = response.places;
         }
 
         function findDetailsById(id) {
