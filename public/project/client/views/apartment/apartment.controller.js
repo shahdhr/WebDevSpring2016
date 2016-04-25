209dd3d4bb07bd168df6d4c86531edec6717400b
Dhruv Shah <dhruvsshah@gmail.com>
2016-04-25 09:55:54 -0400

    Form Validation

--- public/project/client/views/apartment/apartment.controller.js
+++ public/project/client/views/apartment/apartment.controller.js
@@ -35,16 +35,75 @@
 
         function addApartment(apartment)
         {
-            console.log(vm.checked);
+
             var currentUser = UserService.getCurrentUser();
             if(currentUser){
-                if(apartment) {
-                    apartment.owner_id = currentUser._id;
-                    apartment.amenities = checkedAmenities;
-                    ApartmentService
-                        .addApartment(apartment)
-                        .then(addApartmentCallback);
+                var validated = true;
+                $('#title-group').removeClass('has-error');
+                $('#description-group').removeClass('has-error');
+                $('#amenities-group').removeClass('has-error');
+                $('#street-address-group').removeClass('has-error');
+                $('#city-group').removeClass('has-error');
+                $('#country-group').removeClass('has-error');
+                $('#zip-group').removeClass('has-error');
+                $('#bedrooms-group').removeClass('has-error');
+                $('#bathrooms-group').removeClass('has-error');
+                if(!apartment) {
+                    $('#title-group').addClass('has-error');
+                    $('#description-group').addClass('has-error');
+                    $('#amenities-group').addClass('has-error');
+                    $('#street-address-group').addClass('has-error');
+                    $('#city-group').addClass('has-error');
+                    $('#country-group').addClass('has-error');
+                    $('#zip-group').addClass('has-error');
+                    $('#bedrooms-group').addClass('has-error');
+                    $('#bathrooms-group').addClass('has-error');
+                    validated = false;
+                } else {
+                    if(!apartment.title || apartment.title == '') {
+                        $('#title-group').addClass('has-error');
+                        validated = false;
+                    }
+                    if(!apartment.description || apartment.description == '') {
+                        $('#description-group').addClass('has-error');
+                        validated = false;
+                    }
+                    if(!apartment.street || apartment.street == '') {
+                        $('#street-address-group').addClass('has-error');
+                        validated = false;
+                    }
+                    if(!apartment.city || apartment.city == '') {
+                        $('#city-group').addClass('has-error');
+                        validated = false;
+                    }
+                    if(!apartment.country || apartment.country == '') {
+                        $('#country-group').addClass('has-error');
+                        validated = false;
+                    }
+                    if(!apartment.zipcode || apartment.zipcode == '') {
+                        $('#zip-group').addClass('has-error');
+                        validated = false;
+                    }
+                    if(!apartment.number_of_bedrooms || apartment.number_of_bedrooms == '') {
+                        $('#bedrooms-group').addClass('has-error');
+                        validated = false;
+                    }
+                    if(!apartment.number_of_bathrooms || apartment.number_of_bathrooms == '') {
+                        $('#bathrooms-group').addClass('has-error');
+                        validated = false;
+                    }
                 }
+
+                if(!validated) {
+                    return;
+                }
+
+                apartment.owner_id = currentUser._id;
+                apartment.amenities = checkedAmenities;
+                ApartmentService
+                    .addApartment(apartment)
+                    .then(addApartmentCallback);
+
             }
         }
 
--- public/project/client/views/users/regsiter.controller.js
+++ public/project/client/views/users/regsiter.controller.js
@@ -17,6 +17,54 @@
 
         //Event handler implementations
         function register (user) {
+            console.log(user);
+            var validated = true;
+            $('#firstName-group').removeClass('has-error');
+            $('#lastName-group').removeClass('has-error');
+            $('#email-group').removeClass('has-error');
+            $('#username-group').removeClass('has-error');
+            $('#password-group').removeClass('has-error');
+            $('#verifyPassword-group').removeClass('has-error');
+
+            if(!user) {
+                $('#firstName-group').addClass('has-error');
+                $('#lastName-group').addClass('has-error');
+                $('#email-group').addClass('has-error');
+                $('#username-group').addClass('has-error');
+                $('#password-group').addClass('has-error');
+                $('#verifyPassword-group').addClass('has-error');
+                validated = false;
+
+            } else {
+                if (!user.firstName || user.firstName == '') {
+                    $('#firstName-group').addClass('has-error');
+                    validated = false;
+                }
+                if (!user.lastName || user.lastName == '') {
+                    $('#lastName-group').addClass('has-error');
+                    validated = false;
+                }
+                if (!user.email || user.email == '') {
+                    $('#email-group').addClass('has-error');
+                    validated = false;
+                }
+                if (!user.username || user.username == '') {
+                    $('#username-group').addClass('has-error');
+                    validated = false;
+                }
+                if (!user.password || user.password == '') {
+                    $('#password-group').addClass('has-error');
+                    validated = false;
+                }
+                if (!user.repassword || user.repassword == '' || user.password != user.repassword) {
+                    $('#verifyPassword-group').addClass('has-error');
+                    validated = false;
+                }
+            }
+            if(!validated) {
+                console.log("not validated");
+                return;
+            }
             UserService.register(user)
                 .then(registerCallback);
         }
